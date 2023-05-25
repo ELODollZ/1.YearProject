9b04c48b57393e8bbbe2d9ce6086311cd05a6aad
ELODollZ <DanielDKN29@gmail.com>
2023-05-25 13:45:22 +0200

    whole folder

---
+++ Scripts/app.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# Author: NyboMønster
+# Import libary list:
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+import time
+import threading
+
+# Variables:
+sensor = 
+sidsteTemp = None
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+# App route paths:
+@socketio.on('hentTemp')
+def hentTemp():
+    time.sleep(0.5)
+    socketio.emit('temp', sidsteTemp)
+@app.route('/')
+def index():
+    return render_template('main.html')
+
+
+# Main Code:
+def readTemp():
+    global sidsteTemp
+    while True:
+        time.sleep(2)
+        try:
+            sidsteTemp = sensor.read()
+        except:
+            sidsteTemp = None
+
+tempThread = threading.Thread(target=readTemp)
+tempThread.start()
+
+
+# Host WebPage:
+if __name__ == '__main__':
+    socketio.run(app, host="0.0.0.0", debug=True)
