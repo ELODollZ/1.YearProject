a000e98b75f174eedaed244ad0f5745c7e1fd3c4
ELODollZ <DanielDKN29@gmail.com>
2023-06-02 09:29:41 +0200

    RPI Test3

--- bilag/Scripts/FinalSubScriber.bash
+++ bilag/Scripts/FinalSubScriber.bash
@@ -2,25 +2,21 @@
 # Author: NyboMønster
 
 # Variables
-Fullmessages=""
 message=""
-SentMessages="Messages sent"
 IFS=','
 
 # Main Code
 while true
 do
-mosquitto_sub	-h	localhost	-t	"1Year/ESP32Data" -C	1	-R	$Fullmessages | while read messages
+mosquitto_sub	-h	localhost	-t	"1Year/ESP32Data" -C	1	-R	"$messages" | while read -r messages
 do
-	echo $messages
-	echo $Fullmessages
+	echo "$messages"
 	python app.py "$messages"
 done
-
+done
 #
 #Name0 Value0 Name1 Value1 Name2 Value2 Name3 Value3 Name4 Value4 Fixed;
 #do
 #	echo $Name0 $Value0 $Name1 $Value1 $Name2 $Value2 $Name3 $Value3 $Name4 $Value4 $Fixed
 #	messages= $Name0, $Value0, $Name1, $Value1, $Name2, $Value2, $Name3, $Value3, $Name4, $Value4, $Fixed
 #	echo $messages
-#	done
